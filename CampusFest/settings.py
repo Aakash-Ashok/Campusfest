cadca1a879a9a870af5d4d8fa2d8adc07285c0bf
Akash Ashok <83403116+Aakash-Ashok@users.noreply.github.com>
2026-01-02 23:29:59 +0530

    second commit

--- CampusFest/settings.py
+++ CampusFest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_utfr%a$2x-cda#yh8lc!o9k_m#m&s)b#fjoqn-7hh!i@o)-@i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
